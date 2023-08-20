da81d95a110ce382507f12fec7f8c81fc439afed
Gelado047 <juniorthesouza017@gmail.com>
2023-08-20 00:55:32 -0300

    Step two

---
+++ src/index.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int quant;
+    int choice;
+
+    //Quantity
+    int Qrooms= 0, Qpasta= 0, Qburger= 0, Qnoodles= 0, Qshake= 0, Qchicken= 0;
+    // Food items sold
+    int Srooms= 0, Spasta= 0, Sburger= 0, Snoodles= 0, Sshake= 0, Schicken= 0;
+    // Total prace of items
+    int Total_rooms= 0, Total_pasta= 0, Total_burger= 0, Total_noodles= 0, Total_shake= 0, Total_chicken= 0;
+
+
+    cout<<"\n\t Quantity of items we have";
+    cout<<"\n Rooms avaliabel";
+    cin>>Qrooms;
+    cout<<"\n Quantity of pasta: ";
+    cin>>Qpasta;
+    cout<<"\n Quantity of burger: ";
+    cin>>Qburger;
+    cout<<"\n Quantity of noodles: ";
+    cin>>Qnoodles;
+    cout<<"\n Quantity of shake: ";
+    cin>>Qshake;
+    cout<<"\n Quantity of chicken: ";
+    cin>>Qchicken;
+
+}
